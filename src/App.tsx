b43e1cda989a68c0f7ec148ba7da4d70fbd106fc
dylanperer <dylan.perer29@protonmail.com>
2023-03-11 20:16:10 +1300

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import { SignIn } from './pages/auth/SignIn'
-import styled from "styled-components";
-import {Preloader} from "./pages/misc/Preloader";
+import React from 'react';
+import { SignIn } from './pages/auth/SignIn';
+import styled from 'styled-components';
+import { Preloader } from './pages/misc/Preloader';
 
 const AppContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
-    height:100%;
+    height: 100%;
 `;
 
 function App() {
     const [isPreloadReady, setIsPreloadReady] = React.useState<boolean>(false);
-    const onPreloadReady = async ()=>{
-        setIsPreloadReady(true)
+    const onPreloadReady = async () => {
+        setIsPreloadReady(true);
     };
     return (
         <AppContainer className="App">
             <head>
-                <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet" onLoad={()=>setTimeout(onPreloadReady, 6000)}/>
+                <link
+                    href="https://fonts.cdnfonts.com/css/inter"
+                    rel="stylesheet"
+                    onLoad={() => setTimeout(onPreloadReady, 6000)}
+                />
                 <title>anthera</title>
             </head>
-            {isPreloadReady? <SignIn /> : <Preloader/>}
+            {isPreloadReady ? <SignIn /> : <Preloader />}
         </AppContainer>
-    )
+    );
 }
 
-export default App
+export default App;
